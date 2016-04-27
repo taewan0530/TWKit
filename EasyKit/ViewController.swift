cedf7e660c02f3d5a43a6566cdeaba8d9d0ccda1
taewan0530 <kimtaewan@kimtaewan-ui-Mac-mini.local>
2016-04-27 12:26:45 +0900

    toAttributedString 기능 추가

--- EasyKit/ViewController.swift
+++ EasyKit/ViewController.swift
@@ -10,8 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: IBLabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        label.text = "label,Label,label,Label\nlabel,Label,lab-{@image}-el,Label\nlabel,Label,label,Label\nlabel,Label,l-{@image}-abel,Label@@{@icon}@----Label---{@icon}---"
+        label.sizeToFit()
+        if let text = label.text {
+            let img = UIImage(named: "bg_2")!
+            let start = currentTimeMillis()
+            let attributeString = text.toAttributedString(attributesWithKey:
+                [
+                    "Lab": [NSForegroundColorAttributeName : UIColor.brownColor()],
+                    "abe": [
+                        NSForegroundColorAttributeName : UIColor.blueColor(),
+                        NSBackgroundColorAttributeName : UIColor.whiteColor()
+                    ],
+                    "{@image}": [TWKitUIImageAttributeName: img],
+//                    "{@icon}": [
+//                        TWKitUIImageAttributeName: UIImage(named: "pic_where")!,
+//                        TWKitUIImageOffsetYAttributeName: 2
+//                    ]
+                ])
+            
+            let end = currentTimeMillis()
+            print(end-start)
+            label.attributedText = attributeString
+        }
+        //[NSForegroundColorAttributeName : Constants.mainColor()]
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +46,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func currentTimeMillis() -> Int64{
+        let nowDouble = NSDate().timeIntervalSince1970
+        return Int64(nowDouble*1000)
+    }
 
 }
 
--- Pod/extensions/String+Extension.swift
+++ Pod/extensions/String+Extension.swift
@@ -7,6 +7,11 @@
 //
 
 import Foundation
+import UIKit
+
+public typealias TWKitAttributesWithKey = [String: [String: AnyObject]]
+public let TWKitUIImageAttributeName: String = "TWKitUIImageAttributeName"
+public let TWKitUIImageOffsetYAttributeName: String = "TWKitUIImageOffsetYAttributeName"
 
 public extension String {
     public subscript (i: Int) -> Character {
@@ -23,6 +28,55 @@ public extension String {
     
     public func trim() -> String {
         return self.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+    }
+    
+    public func toAttributedString(attributesWithKey attrs: TWKitAttributesWithKey) -> NSAttributedString{
+        let searchAttr = NSMutableAttributedString(string: self)
+        var replacesOffset = 0
+        for (key, value) in attrs {
+            let ranges = searchAttr.string.rangesOfString(key)
+            for range in ranges {
+                //이미지를 포함하고 있는건 리플레이스!
+                if let image = value[TWKitUIImageAttributeName] as? UIImage {
+                    let imageSize = image.size
+                    var bounds = CGRectMake(0, 0, imageSize.width, imageSize.height)
+                    let newRange = NSMakeRange(range.location - replacesOffset, range.length)
+                    
+                    //x는 적용해도 반응이 없네.
+                    bounds.origin.y -= value[TWKitUIImageOffsetYAttributeName] as? CGFloat ?? 0
+                    
+                    
+                    let attachment = NSTextAttachment()
+                    attachment.image = image
+                    attachment.bounds = bounds
+                    
+                    let imageAttrString = NSAttributedString(attachment: attachment)
+                    searchAttr.replaceCharactersInRange(newRange, withAttributedString: imageAttrString)
+                    
+                    //range를 미리 가져오기때문에 offset으로 밀어내준다.
+                    replacesOffset += (range.length-1)
+                } else {
+                    searchAttr.addAttributes(value, range: range)
+                }
+            }
+            replacesOffset = 0
+        }
+        return searchAttr
+    }
+    
+    public func rangesOfString(searchString: String, options mask: NSStringCompareOptions = .LiteralSearch) -> [NSRange] {
+        let nsStr = self as NSString
+        var results = [NSRange]()
+        var searchRange = NSMakeRange(0, nsStr.length);
+        var range = nsStr.rangeOfString(searchString, options: mask, range: searchRange)
+        
+        while range.location != NSNotFound {
+            results.append(range)
+            searchRange = NSMakeRange(NSMaxRange(range), nsStr.length - NSMaxRange(range))
+            range = nsStr.rangeOfString(searchString, options: mask, range: searchRange)
+        }
+        return results
+        
     }
 }
 
